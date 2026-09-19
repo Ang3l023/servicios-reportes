e5edaf0928c203edbb3804ef24433433c8eeeac5
Angelo Rojas <angelo030701@gmail.com>
2026-09-18 22:26:03 -0500

    fix: Soporte Honor Motorola

--- api/share-target.js
+++ api/share-target.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  // Redirigir siempre mediante GET (303) a la pantalla de Angular
   res.writeHead(303, { Location: '/share-target?fromShare=true' });
   res.end();
 }
